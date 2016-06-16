398c6319f8c2271b0d4c8dc622edbf0be8a6c8ed
Jericho Masigan <jco.masigan@gmail.com>
2016-06-16 21:18:49 +1200

    update

--- Main.cs
+++ Main.cs
@@ -8,6 +8,7 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Colors;
 
 namespace ProfileViewer
 {
@@ -73,10 +74,12 @@ namespace ProfileViewer
             Editor ed = doc.Editor;
             using (Transaction tr = db.TransactionManager.StartTransaction())
             {
-                List<ObjectId> contoursId = new List<ObjectId>();
+                List<ObjectId> contoursId = new List<ObjectId>();              
                 ObjectIdCollection majorEnts = SelectByLayer(majorContLayer);
                 ObjectIdCollection minorEnts = SelectByLayer(minorContLayer);
                 List<Tuple<double, double>> intersectDist = new List<Tuple<double, double>>();
+                double maxElevation = 0;
+                double profileLength = 0;
                 foreach (ObjectId id in majorEnts)
                 {
                     using (DocumentLock doclock = doc.LockDocument())
@@ -86,7 +89,14 @@ namespace ProfileViewer
                         Polyline ent = tr.GetObject(profileLine, OpenMode.ForRead) as Polyline;
                         Polyline ent2 = tr.GetObject(id, OpenMode.ForWrite) as Polyline;
                         double elav = ent2.Elevation; //store elevation temporary
+
                         ent2.Elevation = 0;
+
+                        if (maxElevation < elav)
+                        {
+                            maxElevation = elav;
+                        }
+                        profileLength = ent.Length;
 
                         /// SET ELEVATION TO ZERO TEMPORARY THEN PUT IT BACk
                         Point3dCollection intersectionPoints = new Point3dCollection();
@@ -98,6 +108,7 @@ namespace ProfileViewer
                         {
                             double distance = ent.GetDistAtPoint(pt);
                             Tuple<double, double> xsectPt = new Tuple<double, double>(distance, elav);
+
                             intersectDist.Add(xsectPt);
                             //ed.WriteMessage(string.Format("\n INTERSECTION DIST AT : {0}", distance.ToString()));
                            //  FOR TESTING
@@ -115,13 +126,24 @@ namespace ProfileViewer
                         ent2.Elevation = elav; //set it back
                     }
                 }
-                intersectDist.Sort();
+                intersectDist.Sort(); //sort to get the start and end points
+                double profileStartY = intersectDist[0].Item2;
+                double profileEndY = intersectDist[intersectDist.Count - 1].Item2;
+                Tuple<double, double> xsectStart = new Tuple<double, double>(0, profileStartY); //start of profile
+                Tuple<double, double> xsectEnd = new Tuple<double, double>(profileLength, profileEndY); //end of profile
+                intersectDist.Add(xsectStart);
+                intersectDist.Add(xsectEnd);
+
+                intersectDist.Sort(); //have to resort after adding new points
                 Polyline profile = new Polyline();
                 for (int i = 0; i < intersectDist.Count; i++)
                 {
-                    
+
 
                     profile.AddVertexAt(i, new Point2d(intersectDist[i].Item1 + GlobalVars.insertionPt.X, intersectDist[i].Item2 + GlobalVars.insertionPt.Y), 0, 0, 0);
+                    double elevationGrid = DrawLine(new Point3d(intersectDist[i].Item1 + GlobalVars.insertionPt.X, intersectDist[i].Item2 + GlobalVars.insertionPt.Y, 0), new Point3d(intersectDist[i].Item1 + GlobalVars.insertionPt.X, GlobalVars.insertionPt.Y, 0), "PROFILEVIEWER.GRIDLINES");
+                    decimal elevLabel = Convert.ToDecimal(intersectDist[i].Item2);
+                    DrawText(new Point2d(intersectDist[i].Item1 + GlobalVars.insertionPt.X, GlobalVars.insertionPt.Y), GlobalVars.textHeight, 1.5708, elevLabel.ToString("#.##"), "PROFILEVIEWER.LABELS");
                     if(i == intersectDist.Count - 1)
                     {
                         using (DocumentLock docLock = doc.LockDocument())
@@ -132,10 +154,91 @@ namespace ProfileViewer
                         }
                     }
                 }
-                
+                double datumY = profileLength;
+                Polyline guides = new Polyline();
+                guides.AddVertexAt(0, new Point2d(GlobalVars.insertionPt.X, GlobalVars.insertionPt.Y + profileStartY) , 0, 0, 0);
+                guides.AddVertexAt(1, new Point2d(GlobalVars.insertionPt.X, GlobalVars.insertionPt.Y), 0, 0, 0);
+                guides.AddVertexAt(2, new Point2d(GlobalVars.insertionPt.X + profileLength, GlobalVars.insertionPt.Y), 0, 0, 0);
+                guides.AddVertexAt(3, new Point2d(GlobalVars.insertionPt.X + profileLength, GlobalVars.insertionPt.Y + profileEndY), 0, 0, 0);
+
+                DrawText(new Point2d(GlobalVars.insertionPt.X, GlobalVars.insertionPt.Y + profileStartY), GlobalVars.textHeight, 0, datumY.ToString(), "PROFILEVIEWER.LABELS");
+
+                using (DocumentLock docLock = doc.LockDocument())
+                {
+                    BlockTableRecord btr = (BlockTableRecord)tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite);
+                    btr.AppendEntity(guides);
+                    tr.AddNewlyCreatedDBObject(guides, true);
+                }
                 tr.Commit();
             }
          }
+        private static void CreateLayer(string layerName, short colour)
+        {
+
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database currentDB = doc.Database;
+            using (Transaction tr = currentDB.TransactionManager.StartTransaction())
+            {
+                LayerTable layerTable = (LayerTable)tr.GetObject(currentDB.LayerTableId, OpenMode.ForWrite);
+                if (!layerTable.Has(layerName))
+                {
+                    LayerTableRecord layer = new LayerTableRecord();
+                    layer.Name = layerName;
+                    layer.Color = Color.FromColorIndex(ColorMethod.ByLayer, colour);
+                    ObjectId layerID = layerTable.Add(layer);
+                    tr.AddNewlyCreatedDBObject(layer, true);
+                    tr.Commit();
+                }
+            }
+        }
+
+        public static double DrawLine(Point3d pt1, Point3d pt2, string layerName, short colour = 255)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database currentDB = doc.Database;
+            CreateLayer(layerName, colour);
+            Entity entCreated;
+            double actualHeight = 0;
+            using (Transaction tr = currentDB.TransactionManager.StartTransaction())
+            {
+                BlockTable blckTable = tr.GetObject(currentDB.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord blockTableRec = tr.GetObject(blckTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                Line line = new Line();
+                line.StartPoint = pt1;
+                line.EndPoint = pt2;
+                blockTableRec.AppendEntity(line);
+                tr.AddNewlyCreatedDBObject(line, true);
+
+                tr.Commit();
+                return line.Length;
+            }
+        }
+
+        public static void DrawText(Point2d pt1, double textHeight, double rotation, string textVal, string layerName, short colour = 255)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database currentDB = doc.Database;
+            CreateLayer(layerName, colour);
+            Entity entCreated;
+            double actualHeight = 0;
+            using (Transaction tr = currentDB.TransactionManager.StartTransaction())
+            {
+                BlockTable blckTable = tr.GetObject(currentDB.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord blockTableRec = tr.GetObject(blckTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                MText text = new MText();
+                text.Contents = textVal;
+                text.Rotation = rotation;
+                text.TextHeight = textHeight;
+                text.Location = new Point3d(pt1.X, pt1.Y, 0);
+                text.Layer = layerName;
+
+                blockTableRec.AppendEntity(text);
+                tr.AddNewlyCreatedDBObject(text, true);
+                actualHeight = text.ActualHeight;
+                tr.Commit();
+                entCreated = text;
+            }
+        }
         private static List<ObjectId> ObjectIdColl2List(ObjectIdCollection objCol)
         {
             List<ObjectId> list = new List<ObjectId>();
@@ -200,5 +303,6 @@ namespace ProfileViewer
     {
         public static ObjectId profileLineId = ObjectId.Null;
         public static Point3d insertionPt = new Point3d();
+        public static double textHeight = 1;
     }
 }
